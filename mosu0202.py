aa222849bc0edf60c05523f11fd26c7c7540de4a
Mosu1025 <148863720+Mosu1025@users.noreply.github.com>
2023-10-24 15:24:15 +0100

    branch 3 file
    this file is for branch 3

---
+++ mosu0202.py
@@ -0,0 +1,11 @@
+#python coding
+#my new file 
+#class 3
+
+print("hello Mohsan")
+#anything
+print("anything i can write")
+
+#for branch 3
+myname = input("what is your name?")
+print("hello:"+myname)
